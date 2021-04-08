8d887659e1f8e488123279511313b2690ded38a6
rl-max <poiroth946@gmail.com>
2021-04-08 14:13:27 +0900

    c51 code-refactoring

--- c51.py
+++ c51.py
@@ -11,27 +11,26 @@ from collections import deque
 
 EPISODES = 10000
 learning_rate = 0.0005
-discount_factor = 0.98
+discount_factor = 0.9
 buffer_size, start_train = 100000, 2000
 batch_size = 32
-min_sprt, max_sprt = -2, 2
+min_sprt, max_sprt = 0, 2
 num_sprt = 51
 interval = (max_sprt - min_sprt)/(num_sprt - 1)
+num_act = 2
 
 class QNet(nn.Module):
     def __init__(self):
         super().__init__()
         self.fc1 = nn.Linear(4, 256)
         self.fc2 = nn.Linear(256, 256)
-        self.act1 = nn.Linear(256, num_sprt)
-        self.act2 = nn.Linear(256, num_sprt)
+        self.acts = [nn.Linear(256, num_sprt) for _ in range(num_act)]
     
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        act1 = F.softmax(self.act1(x), dim=1)
-        act2 = F.softmax(self.act2(x), dim=1)
-        return act1, act2
+        acts = [F.softmax(self.acts[idx](x), dim=1) for idx in range(num_act)]
+        return acts
 
 def mini_batch(buffer):
     batch = random.sample(buffer, batch_size)
@@ -48,17 +47,17 @@ def mini_batch(buffer):
 
 def train(net, target_net, optimizer, buffer):
     obs, acts, rewards, next_obs, done = mini_batch(buffer)
-    next_dist1, next_dist2 = target_net(next_obs)
-    
+    next_actdists = target_net(next_obs)
     support = torch.arange(min_sprt, max_sprt+1e-2, interval).unsqueeze(0)
     supports = support.repeat(batch_size, 1)
     target_sprts = rewards.view(-1, 1) + discount_factor * done.view(-1, 1) * supports
     target_sprts = torch.clamp(target_sprts, min_sprt, max_sprt)
+    next_actvals = torch.stack([expect(next_actdists[idx]) for idx in range(num_act)], dim=0)
+    next_maxact = torch.argmax(next_actvals, dim=0)
     next_dists = []
-    for idx, (a1val, a2val) in enumerate(zip(expect(next_dist1), expect(next_dist2))):
-        if a1val >= a2val: next_dists.append(next_dist1[idx])
-        else:              next_dists.append(next_dist2[idx])
-    next_dists = torch.stack(next_dists, dim=0).float().detach()
+    for idx, maxact in enumerate(next_maxact):
+        next_dists.append(next_actdists[maxact][idx])
+    next_dists = torch.stack(next_dists, dim=0).detach()
     #projection
     target_dists = []
     for supprt, target_sprt, dist in zip(supports, target_sprts, next_dists):
@@ -69,29 +68,25 @@ def train(net, target_net, optimizer, buffer):
             proportion = (interval - diff)/interval
             t_d = np.array(dist[idx] * proportion)
             sub_dists.append(t_d)
-        target_dist = np.sum(np.array(sub_dists), axis=0)
-        target_dists.append(target_dist)
+        t_dist = np.sum(np.array(sub_dists), axis=0)
+        target_dists.append(t_dist)
     target_dists = torch.tensor(target_dists).float()
-    
     dists = []
-    a1_dists, a2_dists = net(obs)
-    for a1dist, a2dist, act in zip(a1_dists, a2_dists, acts):
-        if act.item() == 0:  dists.append(a1dist)
-        else:                dists.append(a2dist)
+    actdists = net(obs)
+    for idx, act in enumerate(acts):
+        dists.append(actdists[act][idx])
     dists = torch.stack(dists, dim=0).float()
     
     loss = F.kl_div(dists, target_dists.detach())
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
-      
+    
 def expect(dists): #function calculating expectation value
-    q = []
-    for dist in dists:
-        support = torch.arange(min_sprt, max_sprt+1e-2, interval)
-        q_val = torch.sum(support * dist)
-        q.append(q_val)
-    return torch.tensor(q).float()
+    support = torch.arange(min_sprt, max_sprt+1e-2, interval)
+    supports = support.repeat(len(dists), 1)
+    q_val = torch.sum(supports * dists, dim=1)
+    return q_val
 
 if __name__ == '__main__':
     env = gym.make('CartPole-v1')
@@ -102,21 +97,21 @@ if __name__ == '__main__':
     buffer = deque(maxlen=buffer_size)
     score, step = 0.0, 0
     epsilon, epsilon_decay = 0.4, 1-5e-5
-    target_interval = 5
+    target_interval = 10
     
     for ep in range(EPISODES):
         obs = env.reset()
         done = False
         while not done:
-            a1_dist, a2_dist = net(torch.tensor(obs).unsqueeze(0).float())
-            a1_val, a2_val = expect(a1_dist).item(), expect(a2_dist).item()
+            acts_dist = net(torch.tensor(obs).unsqueeze(0).float())
+            acts_val = np.array([expect(acts_dist[idx]).item() for idx in range(num_act)])
             rand = random.random()
             if rand < epsilon:
-                action = random.randint(0, 1)
+                action = random.randint(0, num_act-1)
             else:
-                action = 0 if a1_val >= a2_val else 1
+                action = acts_val.argmax()
             next_obs, reward, done, info = env.step(action)
-            buffer.append((obs, action, reward/10.0, next_obs, done))
+            buffer.append((obs, action, reward/50.0, next_obs, done))
             obs = next_obs
             step += 1
             score += reward
